ee394060f6b65b72057f0a12af21cf54b5bc5f3e
Jarek <jarek.jeda@gmail.com>
2025-02-10 16:15:23 +0100

    initial commit

--- out/build/x64-Debug/third_party/SDL/include/SDL2/SDL_revision.h
+++ out/build/x64-Debug/third_party/SDL/include/SDL2/SDL_revision.h
@@ -2,7 +2,7 @@
 #define SDL_REVISION_NUMBER 0
 
 #ifdef SDL_VENDOR_INFO
-#define SDL_REVISION "SDL-2.28.4-g477ebb2 (" SDL_VENDOR_INFO ")"
+#define SDL_REVISION "SDL-2.28.4-g762d001 (" SDL_VENDOR_INFO ")"
 #else
-#define SDL_REVISION "SDL-2.28.4-g477ebb2"
+#define SDL_REVISION "SDL-2.28.4-g762d001"
 #endif
