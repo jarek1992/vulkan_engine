c3fd94fa3fcd4f4a24f7c8d6663b4101f6988512
Jarek <jarek.jeda@gmail.com>
2025-02-10 13:16:57 +0100

    initial commit

---
+++ out/build/x64-Debug/third_party/SDL/cmake_uninstall.cmake
@@ -0,0 +1,18 @@
+if (NOT EXISTS "D:/vulkan-guide-starting-point-2/out/build/x64-Debug/install_manifest.txt")
+    message(FATAL_ERROR "Cannot find install manifest: \"D:/vulkan-guide-starting-point-2/out/build/x64-Debug/install_manifest.txt\"")
+endif(NOT EXISTS "D:/vulkan-guide-starting-point-2/out/build/x64-Debug/install_manifest.txt")
+
+file(READ "D:/vulkan-guide-starting-point-2/out/build/x64-Debug/install_manifest.txt" files)
+string(REGEX REPLACE "\n" ";" files "${files}")
+foreach (file ${files})
+    message(STATUS "Uninstalling \"$ENV{DESTDIR}${file}\"")
+    execute_process(
+        COMMAND D:/Program Files/Microsoft Visual Studio/2022/Professional/Common7/IDE/CommonExtensions/Microsoft/CMake/CMake/bin/cmake.exe -E remove "$ENV{DESTDIR}${file}"
+        OUTPUT_VARIABLE rm_out
+        RESULT_VARIABLE rm_retval
+    )
+    if(NOT ${rm_retval} EQUAL 0)
+        message(FATAL_ERROR "Problem when removing \"$ENV{DESTDIR}${file}\"")
+    endif (NOT ${rm_retval} EQUAL 0)
+endforeach(file)
+
