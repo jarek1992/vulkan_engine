affda3e0ec17d2befff4186dae966a5edb439d8d
Jarek <jarek.jeda@gmail.com>
2025-02-18 10:49:09 +0100

    init_vulkan/vmaDestroyAllocator

--- src/vk_engine.cpp
+++ src/vk_engine.cpp
@@ -105,6 +105,18 @@ void VulkanEngine::init_vulkan() {
     //use vkbootstrap to get a Graphics queue
     _graphicsQueue = vkbDevice.get_queue(vkb::QueueType::graphics).value();
     _graphicsQueueFamily = vkbDevice.get_queue_index(vkb::QueueType::graphics).value();
+
+    //initialize the memory allocator
+    VmaAllocatorCreateInfo allocatorInfo = {};
+    allocatorInfo.physicalDevice = _chosenGPU;
+    allocatorInfo.device = _device;
+    allocatorInfo.instance = _instance;
+    allocatorInfo.flags = VMA_ALLOCATOR_CREATE_BUFFER_DEVICE_ADDRESS_BIT;
+    vmaCreateAllocator(&allocatorInfo, &_allocator);
+
+    _mainDeletionQueue.push_function([&]() {
+        vmaDestroyAllocator(_allocator);
+        });
 }
 
 void VulkanEngine::init_commands() {
