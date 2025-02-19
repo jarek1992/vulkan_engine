9a5e8a7222bf21bd03e3dcef194bc2d9e7cdcb99
Jarek <jarek.jeda@gmail.com>
2025-02-19 13:08:52 +0100

    header/struct_DecriptorAllocator

--- src/vk_descriptors.h
+++ src/vk_descriptors.h
@@ -10,3 +10,19 @@ struct DescriptorLayoutBuilder {
 	void clear();
 	VkDescriptorSetLayout build(VkDevice device, VkShaderStageFlags shaderStages, void* pNext = nullptr, VkDescriptorSetLayoutCreateFlags flags = 0);
 };
+
+struct DecriptorAllocator {
+
+	struct PoolSizeRatio {
+		VkDescriptorType type;
+		float ratio;
+	};
+
+	VkDescriptorPool pool;
+
+	void init_pool(VkDevice device, uint32_t maxSets, std::span<PoolSizeRatio> poolRatios);
+	void clear_descriptors(VkDevice device);
+	void destroy_pool(VkDevice device);
+
+	VkDescriptorSet allocate(VkDevice device, VkDescriptorSetLayout layout);
+};
