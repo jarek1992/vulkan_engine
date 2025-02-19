d5f8b9056b65de96b9f370f930f2a8fd9f511775
Jarek <jarek.jeda@gmail.com>
2025-02-19 16:38:44 +0100

    void VulkanEngine::init_descriptors()

--- out/build/x64-Debug/third_party/SDL/include/SDL2/SDL_revision.h
+++ out/build/x64-Debug/third_party/SDL/include/SDL2/SDL_revision.h
@@ -2,7 +2,7 @@
 #define SDL_REVISION_NUMBER 0
 
 #ifdef SDL_VENDOR_INFO
-#define SDL_REVISION "SDL-2.28.4-gb0b47ee (" SDL_VENDOR_INFO ")"
+#define SDL_REVISION "SDL-2.28.4-g902e5eb (" SDL_VENDOR_INFO ")"
 #else
-#define SDL_REVISION "SDL-2.28.4-gb0b47ee"
+#define SDL_REVISION "SDL-2.28.4-g902e5eb"
 #endif
