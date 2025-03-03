99cb4f576250f89c603c4dc886081911f42a3d42
Jarek <jarek.jeda@gmail.com>
2025-03-03 12:55:12 +0100

    shaders/colored_triangle_mesh.vert

--- out/build/x64-Debug/third_party/SDL/include/SDL2/SDL_revision.h
+++ out/build/x64-Debug/third_party/SDL/include/SDL2/SDL_revision.h
@@ -2,7 +2,7 @@
 #define SDL_REVISION_NUMBER 0
 
 #ifdef SDL_VENDOR_INFO
-#define SDL_REVISION "SDL-2.28.4-gb13f346 (" SDL_VENDOR_INFO ")"
+#define SDL_REVISION "SDL-2.28.4-ga762b04 (" SDL_VENDOR_INFO ")"
 #else
-#define SDL_REVISION "SDL-2.28.4-gb13f346"
+#define SDL_REVISION "SDL-2.28.4-ga762b04"
 #endif
